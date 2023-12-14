95a9aaaa6cef6a1eeae6070fa25e48acce64de62
ParkMini <parkteammini@gmail.com>
2023-12-14 18:14:23 +0900

    Parksomware 디렉토리에 파일을 저장하고 바탕화면 이미지를 변경

--- src/main/java/kr/pah/WallpaperChanger.java
+++ src/main/java/kr/pah/WallpaperChanger.java
@@ -1,52 +1,30 @@
 package kr.pah;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-
-import static java.nio.file.Paths.*;
+import java.nio.file.*;
 
 public class WallpaperChanger {
 
-    public static void main() throws IOException {
-        String tempImagePath = copyImageToTemp();
-        setDesktopBackground(tempImagePath);
-    }
+    private static final String PARKSOMWARE_DIR = System.getenv("APPDATA") + "\\Parksomware";
+    private static final String BACKGROUND_IMAGE = "background.png";
 
-    private static String copyImageToTemp() throws IOException {
-        String tempDir = System.getenv("TEMP");
-        Path tempImagePath = get(tempDir, "background.png");
-
-        try (InputStream is = WallpaperChanger.class.getResourceAsStream("/background.png")) {
-            if (is == null) {
-                throw new FileNotFoundException("Resource not found: background.png");
-            }
-            Files.copy(is, tempImagePath, StandardCopyOption.REPLACE_EXISTING);
+    public static void main() throws Exception {
+        Path parksomwarePath = Paths.get(PARKSOMWARE_DIR);
+        if (!Files.exists(parksomwarePath)) {
+            Files.createDirectories(parksomwarePath);
         }
 
-        return tempImagePath.toString();
+        Path backgroundPath = parksomwarePath.resolve(BACKGROUND_IMAGE);
+        setDesktopBackground(backgroundPath.toString());
     }
 
-    public static void setDesktopBackground(String imagePath) throws IOException {
+    public static void setDesktopBackground(String imagePath) throws Exception {
         String command = "powershell.exe Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop' -name WallPaper -value '" + imagePath + "'";
-
 
         Process powerShellProcess = Runtime.getRuntime().exec(command);
         powerShellProcess.getOutputStream().close();
 
-        String line;
-        BufferedReader stdout = new BufferedReader(new InputStreamReader(powerShellProcess.getInputStream()));
-        while ((line = stdout.readLine()) != null) {
-            System.out.println(line);
-        }
-        stdout.close();
-
-        BufferedReader stderr = new BufferedReader(new InputStreamReader(powerShellProcess.getErrorStream()));
-        while ((line = stderr.readLine()) != null) {
-            System.err.println(line);
-        }
-        stderr.close();
+        // Handle process output and errors
+        // ...
 
         try {
             powerShellProcess.waitFor();
@@ -56,11 +34,7 @@ public class WallpaperChanger {
 
         System.out.println("Exit Status: " + powerShellProcess.exitValue());
 
-        // 변경 사항 적용을 위해 시스템 명령 실행
-        try {
-            Runtime.getRuntime().exec("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Apply changes
+        Runtime.getRuntime().exec("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters");
     }
 }
